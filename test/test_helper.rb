db5accb4ffcea64c4c81b6d470b878822ae63a0d
Shlomi Zadok <shlomi@ben-hanna.com>
2016-04-17 15:28:51 +0300

    Refs #14621
    Adds APP_ROOT as constant to plugin.
    Though it exists in Smart-Proxy, it may not be respected (e.g., when running tests)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,4 @@ require 'smart_proxy_for_testing'
 
 # create log directory in our (not smart-proxy) directory
 FileUtils.mkdir_p File.dirname(Proxy::SETTINGS.log_file)
+APP_ROOT = "#{File.dirname(__FILE__)}/.."
