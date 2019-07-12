10ee8acd30976422c437788f126c194ca062a8ff
Ondrej Prazak <oprazak@redhat.com>
2019-07-12 13:24:22 +0200

    Fixes #27287 - Use parser gem for ds file validations

--- lib/smart_proxy_openscap/content_parser.rb
+++ lib/smart_proxy_openscap/content_parser.rb
@@ -1,30 +1,24 @@
-require 'smart_proxy_openscap/shell_wrapper'
+require 'openscap_parser/datastream_file'
+require 'openscap_parser/tailoring_file'
 
 module Proxy::OpenSCAP
-  class ContentParser < ShellWrapper
-    def initialize(type)
-      @type = type
-      @script_name = 'smart-proxy-scap-validation'
-    end
-
-    def validate(scap_file)
-      execute_shell_command scap_file
-    end
-
-    def out_filename
-      "#{in_filename}json-"
-    end
-
-    def in_filename
-      "#{super}-#{@type}-validate-"
-    end
-
-    def failure_message
-      "Failure when running script which validates scap files"
-    end
-
-    def command(in_file, out_file)
-      "#{script_location} #{in_file.path} #{out_file.path} #{@type}"
+  class ContentParser
+    def validate(file_type, scap_file)
+      msg = 'Invalid SCAP file type'
+      errors = []
+      file = nil
+      begin
+        case file_type
+        when 'scap_content'
+          file = ::OpenscapParser::DatastreamFile.new(scap_file)
+        when 'tailoring_file'
+          file = ::OpenscapParser::TailoringFile.new(scap_file)
+        end
+        errors << msg unless file.valid?
+      rescue Nokogiri::XML::SyntaxError => e
+        errors << msg
+      end
+      { errors: errors }
     end
   end
 end
