469d2f619426630b931f6d1df98c57538acc2385
Ondrej Prazak <oprazak@redhat.com>
2021-02-08 11:09:23 +0100

    Fixes #31846 - Remove ruby-openscap

--- lib/smart_proxy_openscap/policy_guide.rb
+++ lib/smart_proxy_openscap/policy_guide.rb
@@ -1,22 +1,26 @@
-require 'openscap'
-require 'openscap/source'
-require 'openscap/ds/sds'
-require 'json'
+require 'smart_proxy_openscap/openscap_exception'
 
 module Proxy
   module OpenSCAP
     class PolicyGuide
-      def generate_guide(in_file, out_file, policy=nil)
-        ::OpenSCAP.oscap_init
-        source = ::OpenSCAP::Source.new in_file
-        sds = ::OpenSCAP::DS::Sds.new source
-        sds.select_checklist
-        html = sds.html_guide policy
-        File.write(out_file, { :html => html.force_encoding('UTF-8') }.to_json)
+      include ::Proxy::Log
+
+      def generate_guide(file_content, policy_id)
+        file = Tempfile.new
+        file.write file_content
+        file.rewind
+        `oscap xccdf generate #{profile_opt policy_id} guide #{file.path}`
+      rescue => e
+        logger.debug e.message
+        logger.debug e.backtrace.join("\n\t")
+        raise OpenSCAPException, "Failed to generate policy guide, cause: #{e.message}"
       ensure
-        sds.destroy if sds
-        source.destroy if source
-        ::OpenSCAP.oscap_cleanup
+        file.close
+        file.unlink
+      end
+
+      def profile_opt(policy_id)
+        policy_id ? "--profile #{policy_id}" : ''
       end
     end
   end
