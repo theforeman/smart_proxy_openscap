62aad568d562cfcb81f5df0ee6473a6e0c890550
Adam Ruzicka <aruzicka@redhat.com>
2024-11-06 15:07:21 +0000

    Do not let oscap --help leak to stdout on proxy startup

--- lib/smart_proxy_openscap/validate_settings.rb
+++ lib/smart_proxy_openscap/validate_settings.rb
@@ -3,7 +3,7 @@ require 'open3'
 module Proxy::OpenSCAP
   class ValidateSettings < ::Proxy::PluginValidators::Base
     def validate!(_settings)
-      Open3.popen2(['oscap', '--help'])
+      _, _, _ = Open3.popen3(['oscap', '--help'])
     rescue Errno::ENOENT
       raise FileNotFound.new("'oscap' utility is not available")
     end
