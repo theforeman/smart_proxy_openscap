60e6de9a6bdbd9861dd25db9b0a84e5ed747e6c1
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-11-11 15:21:54 +0100

    Drop old Ruby compatibility code
    Since dcd9b6be9708287b621191be9e954d65d4767851 version 2.7 is the lowest
    support version.
    
    Fixes: dcd9b6be9708 ("Use GHA for CI (#96)")

--- lib/smart_proxy_openscap/arf_html.rb
+++ lib/smart_proxy_openscap/arf_html.rb
@@ -13,8 +13,6 @@ module Proxy
 
       def get_arf_html(file_in)
         arf_object = ::OpenSCAP::DS::Arf.new(file_in)
-        # @TODO: Drop this when support for 1.8.7 ends
-        return arf_object.html if RUBY_VERSION.start_with? '1.8'
         arf_object.html.force_encoding('UTF-8')
       end
     end
