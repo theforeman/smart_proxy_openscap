theforeman/smart_proxy_openscap

commit cab14f21467632d0429b23ea7461f5b68619eaa2

Respect SMART_PROXY_BRANCH for better CI support
The GitHub Action workflow provides this environment variable and makes
it easy to test a PR in this plugin against a smart-proxy PR.

Gemfile
@@ -6,7 +6,7 @@ group :development do
   gem 'pry'
   gem 'pry-byebug'
   gem 'rubocop'
-  gem 'smart_proxy', :github => "theforeman/smart-proxy", :branch => 'develop'
+  gem 'smart_proxy', :github => "theforeman/smart-proxy", :branch => ENV['SMART_PROXY_BRANCH']
 end
 
 # load local gemfile

commit 78bbcb3d325c749530dc719765518ac31261853e

Support bundler < 2.3 again
Bundler 2.3 has learned to deal with git repos that name their branch
something other than develop, but only Ruby 3.1 started to ship that
version.

Fixes: cab14f214676 ("Respect SMART_PROXY_BRANCH for better CI support")

Gemfile
@@ -6,7 +6,7 @@ group :development do
   gem 'pry'
   gem 'pry-byebug'
   gem 'rubocop'
-  gem 'smart_proxy', :github => "theforeman/smart-proxy", :branch => ENV['SMART_PROXY_BRANCH']
+  gem 'smart_proxy', :github => "theforeman/smart-proxy", :branch => ENV.fetch('SMART_PROXY_BRANCH', 'develop')
 end
 
 # load local gemfile